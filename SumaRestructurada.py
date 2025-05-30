ca5c344038ebc1a4b5e04e1ece5bc28f7a68e5bf
Avila Oziel <oziavilague@gmail.com>
2025-05-30 09:29:07 -0600

    Add files via upload

---
+++ SumaRestructurada.py
@@ -0,0 +1,16 @@
+def datos():
+    print("Bienvenido a la suma de dos numeros")
+    a=float(input("Ingrese el primer valor\n"))
+    b=float(input("ingrese el segundo numero"))
+   
+    resul=a+b 
+    print(resul)
+
+
+
+
+
+
+
+
+datos()
